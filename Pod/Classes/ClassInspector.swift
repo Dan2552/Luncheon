4543c13c7f7a414e253e7d087747f355bdcac4a4
Daniel Green <dan2552@gmail.com>
2015-07-08 23:46:23 +0100

    Add PropertyType and propertyTypes to ClassInspector

--- Pod/Classes/ClassInspector.swift
+++ Pod/Classes/ClassInspector.swift
@@ -9,8 +9,15 @@
 import ObjectiveC
 import UIKit
 
-class ClassInspector {
-    class func properties(classToInspect: AnyClass) -> [String] {
+public enum PropertyType: String {
+    case NSString = "NSString"
+    case NSDate = "NSDate"
+    case BOOL = "BOOL"
+    case Other = "Other"
+}
+
+public class ClassInspector {
+    public class func properties(classToInspect: AnyClass) -> [String] {
         var count : UInt32 = 0
         let properties : UnsafeMutablePointer <objc_property_t> = class_copyPropertyList(classToInspect, &count)
         var propertyNames = [String]()
@@ -33,5 +40,37 @@ class ClassInspector {
         }
         
         return propertyNames
+    }
+    
+    public class func propertyTypes(classToInspect: AnyClass) -> [String: PropertyType] {
+        var count : UInt32 = 0
+        let properties : UnsafeMutablePointer <objc_property_t> = class_copyPropertyList(classToInspect, &count)
+        var propertyTypes = [String: PropertyType]()
+        let propertyCount = Int(count)
+        for i in 0..<propertyCount {
+            let property : objc_property_t = properties[i]
+            let propertyName = NSString(UTF8String: property_getName(property))!
+            let propertyAttr = NSString(UTF8String: property_getAttributes(property))!
+            
+            propertyTypes[propertyName as String] = propertyNameToPropertyType(propertyAttr as String)
+        }
+        free(properties)
+        
+        if let superclass: AnyClass = classToInspect.superclass() {
+            if superclass.conformsToProtocol(Lunch) {
+                for (key, value) in self.propertyTypes(superclass) {
+                    propertyTypes[key] = value
+                }
+            }
+        }
+        
+        return propertyTypes
+    }
+    
+    private class func propertyNameToPropertyType(name: String) -> PropertyType {
+        if name.hasPrefix("T@\"NSString\"") { return .NSString }
+        if name.hasPrefix("T@\"NSDate\"") { return .NSDate }
+        if name.hasPrefix("TB,") { return .BOOL }
+        return .Other
     }
 }
